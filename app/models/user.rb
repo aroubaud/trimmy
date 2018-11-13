8174c88529077ed54bebcb9b0ede367826df11d4
Alexandre Roubaud <roubaud.alex@gmail.com>
2018-11-13 17:40:19 -0500

    Created seed and updated user destroy dependency with bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :services, through: :bookings
   validates :first_name, presence: true
   validates :last_name, presence: true
