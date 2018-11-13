f47eb71b4e5591915760c8d276523eb81cf7dc58
Nancy Ihirwe <kayitarenancy@gmail.com>
2018-11-13 16:13:27 -0500

    bookings changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,7 @@
 class BookingsController < ApplicationController
   def index
+    @service = Service.find(params[:service_id])
+    @bookings = Booking.all
   end
 
   def show
@@ -9,5 +11,11 @@ class BookingsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:service_id, :user_id, :date, :status)
   end
 end
