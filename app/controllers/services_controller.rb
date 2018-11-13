a9e3ef5b6b797302c3945234443033683ca28b16
luc barika <lucbarika@gmail.com>
2018-11-13 18:22:44 -0500

    to be reviewed, as a user i can view a hairstylist's services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,8 +1,10 @@
 class ServicesController < ApplicationController
   def index
+    @hairstylist = User.find(params[:hairstylist_id])
   end
 
   def show
+    @service = Service.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:index, :show]
+
+  get "hairstylists", to: "users#index", as: :hairstylists
+  # get "users/:id", to: "users#show", as: :user
+  get "hairstylists/:hairstylist_id/services", to: "services#index", as: :hairstylist_services
+
   resources :services do
     resources :bookings, only: [:index, :show, :new, :create]
   end
